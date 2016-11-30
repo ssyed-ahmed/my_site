cba816e358693bd808f3d3b704fba776ff8638ae
Sumair <sumair@coursera.com>
2016-11-30 22:44:05 +1100

    mod files

---
+++ assets/js/restaurant.controller.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular.module('Restaurant')
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['$scope'];
+  function MainController($scope) {
+
+    $scope.menuItems = [
+      {name: 'Home', imgSuffix: 'home', path: 'public.home'},
+      {name: 'Menu', imgSuffix: 'cutlery', path: 'public.menu'},
+      {name: 'Specialities', imgSuffix: 'asterisk', path: 'public.specialities'},
+      {name: 'Testimonials', imgSuffix: 'user', path: 'public.testimonials'},
+      {name: 'Contact Us', imgSuffix: 'earphone', path: 'public.contactus'},
+      {name: 'Admin', imgSuffix: 'lock', path: 'public.admin'}
+    ];
+
+    $scope.activeMenu = $scope.menuItems[0].name;
+
+    $scope.setActive = function(menuItem) {
+      $scope.activeMenu = menuItem.name;
+    };
+  }
+})();
