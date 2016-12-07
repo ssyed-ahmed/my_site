76edb2b281856e957a7d5dd6a19fda8ebe1eeaa4
Sumair <sumair@coursera.com>
2016-12-07 11:41:31 +1100

    mod files

--- assets/admin/admin.controller.js
+++ assets/admin/admin.controller.js
@@ -2,23 +2,44 @@
   'use strict';
 
   angular.module('public')
-    .controller('AdminController', AdminController);
+    .controller('AdminController', AdminController)
+    .controller('ModalInstanceController', ModalInstanceController);
 
-  AdminController.$inject = ['$uibModal', '$timeout'];
-  function AdminController($uibModal, $timeout) {
+  AdminController.$inject = ['$uibModal', '$rootScope'];
+  function AdminController($uibModal, $rootScope) {
     var ctrl = this;
 
-    ctrl.$onInit = function() {
+    //Listen for 'adminClicked' event from the restaurant.controller
+    $rootScope.$on('adminClicked', function(event, args) {
+      // Open admin authentication dialog
       ctrl.openDialog();
-    };
+    });
 
     ctrl.openDialog = function() {
-      $timeout(function () {
-        $uibModal.open({
+
+        var modalInstance = $uibModal.open({
           templateUrl: 'assets/admin/admin.html',
-          size: 'lg'
-        })
-      }, 10);
+          size: 'lg',
+          keyboard: true,
+          backdrop: 'static',
+          controller: 'ModalInstanceController',
+          controllerAs: 'ctrl',
+          bindToController: true
+        });
+
+    };
+  }
+
+  ModalInstanceController.$inject = ['$uibModalInstance'];
+  function ModalInstanceController($uibModalInstance) {
+    var modalCtrl = this;
+
+    modalCtrl.ok = function() {
+      $uibModalInstance.close();
+    };
+
+    modalCtrl.cancel = function() {
+      $uibModalInstance.dismiss();
     };
   }
 })();
--- assets/js/restaurant.controller.js
+++ assets/js/restaurant.controller.js
@@ -4,22 +4,26 @@
   angular.module('Restaurant')
     .controller('MainController', MainController);
 
-  MainController.$inject = ['$scope'];
-  function MainController($scope) {
+  MainController.$inject = ['$scope', '$rootScope'];
+  function MainController($scope, $rootScope) {
 
     $scope.menuItems = [
       {name: 'Home', imgSuffix: 'home', path: 'public.home'},
       {name: 'Menu', imgSuffix: 'cutlery', path: 'public.menu'},
       {name: 'Specialities', imgSuffix: 'asterisk', path: 'public.specialities'},
       {name: 'Testimonials', imgSuffix: 'user', path: 'public.testimonials'},
-      {name: 'Contact Us', imgSuffix: 'earphone', path: 'public.contactus'},
-      {name: 'Admin', imgSuffix: 'lock', path: 'public.admin'}
+      {name: 'Contact Us', imgSuffix: 'earphone', path: 'public.contactus'}
+      //{name: 'Admin', imgSuffix: 'lock', path: 'public.admin'}
     ];
 
     $scope.activeMenu = $scope.menuItems[0];
 
     $scope.setActive = function(menuItem) {
       $scope.activeMenu = menuItem;
+      if (menuItem.name === 'Admin') {
+        //handle admin route
+        $rootScope.$emit('adminClicked', {isAdminAction: true});
+      }
     };
   }
 })();
