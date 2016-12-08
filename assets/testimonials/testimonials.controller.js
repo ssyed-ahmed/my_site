5730d1b294959d025f353759cf59ce477b6224fa
Sumair <sumair@coursera.com>
2016-12-08 20:06:12 +1100

    mod files

--- assets/testimonials/testimonials.controller.js
+++ assets/testimonials/testimonials.controller.js
@@ -4,38 +4,13 @@
   angular.module('public')
     .controller('TestimonialsController', TestimonialsController);
 
-  function TestimonialsController() {
+  TestimonialsController.$inject = ['TestimonialsService'];
+  function TestimonialsController(TestimonialsService) {
       var $ctrl = this;
 
       $ctrl.isFormShown = false;
 
-      var testimonialsList = [
-        {
-          statement: 'Definitely would recommend to others....best food ever!!',
-          email: 'ash44@junk.com',
-          occupation: 'Writer'
-        },
-        {
-          statement: 'Best food I have ever had...',
-          email: 'jhdf355@test.com',
-          occupation: 'Journalist'
-        },
-        {
-          statement: 'Awesome...simply awesome :)',
-          email: 'hut48@kjl.com',
-          occupation: 'Musician'
-        },
-        {
-          statement: 'Would definitely come back again...especially for the kebabs...they rock!!',
-          email: 'genius52@yah.com',
-          occupation: 'Engineer'
-        },
-        {
-          statement: 'One of the best places for kebabs....would highly recommend them to my friends.',
-          email: 'medy22@uet.com',
-          occupation: 'Doctor'
-        }
-      ];
+      var testimonialsList = TestimonialsService.getTestimonials();
 
       $ctrl.getTestimonials = function() {
         return testimonialsList;
---
+++ assets/testimonials/testimonials.service.js
@@ -0,0 +1,42 @@
+(function () {
+  'use strict';
+
+  angular.module('public')
+    .service('TestimonialsService', TestimonialsService);
+
+  function TestimonialsService() {
+    var service = this;
+
+    service.getTestimonials = function() {
+      var testimonialsList = [
+        {
+          statement: 'Definitely would recommend to others....best food ever!!',
+          email: 'ash44@junk.com',
+          occupation: 'Writer'
+        },
+        {
+          statement: 'Best food I have ever had...',
+          email: 'jhdf355@test.com',
+          occupation: 'Journalist'
+        },
+        {
+          statement: 'Awesome...simply awesome :)',
+          email: 'hut48@kjl.com',
+          occupation: 'Musician'
+        },
+        {
+          statement: 'Would definitely come back again...especially for the starters...they rock!!',
+          email: 'genius52@yah.com',
+          occupation: 'Engineer'
+        },
+        {
+          statement: 'One of the best places for the peking duck....would highly recommend that to my friends.',
+          email: 'medy22@uet.com',
+          occupation: 'Doctor'
+        }
+      ];
+
+      return testimonialsList;
+    }
+  }
+})();
