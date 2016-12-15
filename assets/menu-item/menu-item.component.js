56b75f8eaf657eecca1bb5f2a4a3b19468265286
Sumair <sumair@coursera.com>
2016-12-15 19:56:22 +1100

    mod files

--- assets/menu-item/menu-item.component.js
+++ assets/menu-item/menu-item.component.js
@@ -10,7 +10,26 @@
       controller: MenuItemController
     });
 
-    function MenuItemController() {
+    MenuItemController.$inject = ['$filter'];
+    function MenuItemController($filter) {
       var $ctrl = this;
+
+      $ctrl.getMenuLargePortion = function() {
+        var ret = ($ctrl.menuItem.large_portion_name === '' || $ctrl.menuItem.large_portion_name === null)? 'NA': $ctrl.menuItem.large_portion_name;
+        return ret;
+      };
+
+      $ctrl.getMenuSmallPortion = function() {
+        var ret = ($ctrl.menuItem.small_portion_name === '' || $ctrl.menuItem.small_portion_name === null)? 'NA': $ctrl.menuItem.small_portion_name;
+        return ret;
+      };
+
+      $ctrl.getMenuSmallPrice = function() {
+        var ret = ($ctrl.menuItem.price_small === '' || $ctrl.menuItem.price_small === null)? 'NA': $ctrl.menuItem.price_small;
+        if(ret !== 'NA') {
+          ret = $filter('currency')(ret);
+        }
+        return ret;
+      };
     }
 })();
