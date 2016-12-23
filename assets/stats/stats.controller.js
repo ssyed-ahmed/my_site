e6b628eeadee4a274e77b627e527e53b8a29427b
Sumair <sumair@coursera.com>
2016-12-23 11:07:22 +1100

    mod files

--- assets/stats/stats.controller.js
+++ assets/stats/stats.controller.js
@@ -36,4 +36,19 @@
       }
     };
   }
+
+  angular.module('stats')
+    .controller('StatsBarController', StatsBarController);
+
+  function StatsBarController() {
+    var ctrl = this;
+
+    ctrl.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    ctrl.series = ['Series A', 'Series B'];
+
+    ctrl.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+  }
 })();
